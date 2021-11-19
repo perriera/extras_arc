9bf7c1bf2da52c3f27256482d0b0be17d2a75fc2
Perry Anderson <perry.anderson@gmail.com>
2021-11-19 13:49:34 -0800

    Added test case

--- test/imploder/mock_Imploder.cpp
+++ test/imploder/mock_Imploder.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <ng_imploder/imploder/Imploder.hpp>
 #include <extras/filesystem/paths.hpp>
+#include <extras/filesystem/system.hpp>
 
 #include "../vendor/catch.hpp"
 #include "../vendor/fakeit.hpp"
@@ -13,13 +14,16 @@ SCENARIO("Mock ImploderInterface: toOctal", "[ImploderInterface]") {
 
     ng::Filename original = ~extras::Paths("data/exparx.webflow.zip");
     ng::Path originalDir = original + ".dir";
-    ng::Filename imploded = original += "_imploded";
-    ng::Filename exploded = original += "_exploded";
+    ng::Filename imploded = original + "_imploded";
+    ng::Filename exploded = original + "_exploded";
     Mock<ng::ImploderInterface> mock;
     When(Method(mock, original)).Return(original);
     When(Method(mock, imploded)).Return(imploded);
     When(Method(mock, exploded)).Return(exploded);
-    When(Method(mock, unzip)).AlwaysDo([](const ng::Filename&, const ng::Path&) {});
+    When(Method(mock, unzip)).AlwaysDo([](const ng::Filename& filename, const ng::Path& dir) {
+        auto cmd = "unzip -o " + filename + " -d " + dir;
+        SystemException::assertion(cmd.c_str(), __INFO__);
+        });
     When(Method(mock, rezip)).AlwaysDo([](const ng::Filename&, const ng::Path&) {});
     When(Method(mock, implode)).Return();
     When(Method(mock, explode)).Return();
