164c604fc2c04810926c964f5814130705966690
Perry Anderson <perry.anderson@gmail.com>
2021-11-20 00:05:50 -0800

    Added v0.5.0

--- changelog.md
+++ changelog.md
@@ -4,8 +4,8 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), 
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).</br>
 > To add a tag to the git repository do:
-> > git tag -a v0.4.1 -m "Using extras v5.2.0"</br>
-> > git push origin v0.4.1
+> > git tag -a v0.5.0 -m "Added ImploderInterface"</br>
+> > git push origin v0.5.0
 > 
 
 IMPORTANT: 
@@ -28,6 +28,10 @@ Correct URL for github, (for source compare)</br>
 Adding mock test cases </br>
 Adding unit test cases </br>
 Updated README.md accordinging</br>
+
+#### 0.5.0 (2021-11-20)
+### Added 
+- Added ImploderInterface
 
 #### 0.4.1 (2021-11-16)
 ### Added 
@@ -94,7 +98,8 @@ Updated README.md accordinging</br>
 ### Removed
 - Conan package manager deprecated
 
-[Unreleased]: https://github.com/perriera/ng_imploder/compare/v0.4.1...HEAD
+[Unreleased]: https://github.com/perriera/ng_imploder/compare/v0.5.0...HEAD
+[0.5.0]: https://github.com/perriera/ng_imploder/compare/v0.4.1...v0.4.0
 [0.4.1]: https://github.com/perriera/ng_imploder/compare/v0.3.0...v0.4.1
 [0.3.0]: https://github.com/perriera/ng_imploder/compare/v0.2.0...v0.3.0
 [0.2.0]: https://github.com/perriera/ng_imploder/compare/v0.1.0...v0.2.0
--- instances/implode.cpp
+++ instances/implode.cpp
@@ -1,39 +1,19 @@
-// #include "../docopt/include/docopt.h"
-// #include "../include/Imploder/Linux/Imploder.hpp"
-// #include <fstream>
-// #include <iostream>
+#include <ng_imploder/imploder/Imploder.hpp>
+#include <extras/status/StatusLine.hpp>
+#include <iostream>
 
-// using namespace std;
+using namespace  extras;
 
-// static const char USAGE[] =
-//     R"(ChessMind.
-
-// Usage:
-//   chessmind [<name>]
-
-// Options:
-//   -h, --help           Show this screen.
-//   --version            Show version.
-// )";
-
-// int main(int argc, const char *argv[]) {
-//   std::map<std::string, docopt::value> args =
-//       docopt::docopt(USAGE, {argv + 1, argv + argc}, true, "ChessMind 1.0");
-
-//   std::string name = "";
-
-//   if (args["<name>"] && args["<name>"].isString()) {
-//     name = args["<name>"].asString();
-//     cout << name << endl;
-//     ifstream myfile(name);
-//     if (!myfile.good())
-//       cout << "File not found: " << name << endl;
-//     else {
-//       cout << name << " found" << endl;
-//       LinuxImploder imploder(name);
-//       imploder.execute();
-//     }
-//   }
-
-//   return 0;
-// }
+int main(int argc, char const* argv[]) {
+    try {
+        std::cout << extras::start(argv[0]) << std::endl;
+        ng::Imploder imploder(argv[1]);
+        imploder.implode();
+        std::cout << extras::end(argv[0]) << std::endl << std::endl;
+        return 0;
+    }
+    catch (std::exception& ex) {
+        std::cout << extras::fail(ex.what()) << std::endl << std::endl;
+        return -1;
+    }
+}
